aeb95e32b90daa00135e03bbdabcc340bd24f92e
leonali030 <128420049+leonali030@users.noreply.github.com>
2024-03-02 06:04:26 -0800

    Delete src/api directory

--- src/api/yelp.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-export default axios.create({
-    baseURL: 'https://api.yelp.com/v3/businesses',
-    headers: {
-        Authorization: 'Bearer 86-zOJcREmc-Xg-6GpM-7U4_Yrzkt4Hr75L9aViDkkNiWPHJfzNfBkpAUhFZyjBTSLomjw-zQ7nCS_kJx48s9yQudJBtzvUWQBMAMQ0mLN0aBfh4y6NbjZcmgZXgZXYx' 
-    }
-})
